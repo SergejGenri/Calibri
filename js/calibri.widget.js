25f29c2c0629dc46f2deafb2d9b72f723306fd8f
Andrii Povelychenko <anryogo@gmail.com>
2017-02-12 17:30:31 +0200

    replace RequireJS to browserify

--- js/calibri.widget.js
+++ js/calibri.widget.js
@@ -1,40 +1,60 @@
-define([
-    'jquery',
-    'jquery-ui'
-], function ($) {
+require('jsrender');
 
-    $.widget('custom.calibri', {
-        _create: function () {
-            this._build();
-        },
+var TEMPLATES = {
+    login: require('./templates/login.html'),
+    loading: require('./templates/loading.html'),
+    chat: require('./templates/chat.html')
+};
 
-        _build: function () {
-            var self = this;
+$.widget('custom.calibri', {
+    options: {
 
-            self.element.resizable({
-                ghost: true,
-                animate: true,
-                maxHeight: 560,
-                maxWidth: 640,
-                minHeight: 370,
-                minWidth: 270
+    },
+
+    _create: function () {
+        var html = $.templates(TEMPLATES.chat)();
+
+        this.element.addClass('calibri talk-chat');
+        this.element.html(html);
+        this._build();
+    },
+
+    _build: function () {
+        var self = this;
+
+        self.element.resizable({
+            ghost: true,
+            animate: true,
+            maxHeight: 560,
+            maxWidth: 640,
+            minHeight: 370,
+            minWidth: 270
+        });
+
+        self.element.find('.calibri-header').on('mousedown', function () {
+            self.element.draggable({
+                containment: 'document',
+                handle: '.calibri-header'
             });
+        });
 
-            self.element.find('.calibri-header').on('mousedown', function () {
-                self.element.draggable({
-                    containment: 'document',
-                    handle: '.calibri-header'
-                });
-            });
+        self.element.find('.close-reg-chat').on('mousedown', function () {
+            self.element.addClass('hiden');
+        });
 
-            self.element.find('.close-reg-chat').on('mousedown', function () {
-                self.element.addClass('hiden');
-            });
+        // $('.close-talk-chat').on('mousedown', function () {
+        //     $('.talk-chat').addClass('hiden');
+        // });
+    },
 
-            // $('.close-talk-chat').on('mousedown', function () {
-            //     $('.talk-chat').addClass('hiden');
-            // });
-        }
-    });
-
+    _destroy: function () {
+        this.element.resizable("destroy");
+        this.element.draggable("destroy");
+        this.element.removeClass();
+        this.element.empty();
+    }
 });
+
+var elem = $('<div></div>').appendTo(document.body);
+
+module.exports = elem.calibri();
